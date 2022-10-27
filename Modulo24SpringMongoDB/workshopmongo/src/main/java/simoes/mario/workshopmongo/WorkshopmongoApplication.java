e34de3db86559625054f75ab570f8ef33e35cafd
MARIOHELIOSIMOES <mariohelio_simoes@hotmail.com>
2022-10-27 15:00:12 -0300

    Modulo 24 - primeiro commit

---
+++ Modulo24SpringMongoDB/workshopmongo/src/main/java/simoes/mario/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package simoes.mario.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
